99a5ddc36edc8fbc0de81bcdc5dcea3fe33d120f
Dylan Widjaja <dwidjaja945@gmail.com>
2018-04-17 10:33:16 -0700

    worked on yelpData ajax call. Also converted to OOP

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
  * Define all global variables here.
  */
 
-var searchFoodItem;
+var searchFoodTerm;
 var foodLatitude;
 var foodLongitude;
 
@@ -23,19 +23,53 @@ var foodLongitude;
  * Sends request to Yelp API to pull data based off search input from User
  */
 
+class yelpData {
+    constructor(term, latitude, longitude) {
+        this.searchTerm = term;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.yelpAjaxCall = {
+            dataType: "JSONP",
+            method: 'GET',
+            jsonCallBack : this.jsonCallBack(results),
+            // url: `https://api.yelp.com/v3/businesses/search?term=${searchFoodTerm}&latitude=${latitude}&longitude=${longitude}",    // for when we can input search terms and google map lat/long
+            url: "https://api.yelp.com/v3/businesses/search?term=bbq&latitude=34.0522&longitude=-118.2437",
+            data : {
+                "Authorization" : "Bearer 17TJfP0tFmBX3bHRcvUEDnVkR2VgnziO0jhDrwgPcrEJXjJ0H66V0H5kmMWQwTHX2cZfhynFzE3sjaEzBb-v7chrsyweKxQQIvPbbW5SvMZt01-PWWi7PPo2PEvVWnYx",
+                // "term" : "bbq",
+                // "latitude" : 34.0522,
+                // "longitude" : -118.2437,
+            },
+            success : function(results) {
+                console.log("success : " , results);
+            },
+            error : function(errors) {
+                console.log( "errors : " , errors );
+            }
+        }
+    }
+    jsonCallBack(jsonResults) {
+        console.log(jsonResults);
+    },
+
+}
+
 var yelpAjaxCall = {
-    dataType: "JSON",
+    dataType: "JSONP",
     method: 'GET',
-    // url: `https://api.yelp.com/v3/businesses/search?term=${searchFoodItem}&latitude=${latitude}&longitude=${longitude}",
+    // url: `https://api.yelp.com/v3/businesses/search?term=${searchFoodTerm}&latitude=${latitude}&longitude=${longitude}",
     url: "https://api.yelp.com/v3/businesses/search?term=bbq&latitude=34.0522&longitude=-118.2437",
     data : {
         "Authorization" : "Bearer 17TJfP0tFmBX3bHRcvUEDnVkR2VgnziO0jhDrwgPcrEJXjJ0H66V0H5kmMWQwTHX2cZfhynFzE3sjaEzBb-v7chrsyweKxQQIvPbbW5SvMZt01-PWWi7PPo2PEvVWnYx",
+        // "term" : "bbq",
+        // "latitude" : 34.0522,
+        // "longitude" : -118.2437,
     },
     success : function(results) {
-        console.log("success : " + results);
+        console.log("success : " , results);
     },
     error : function(errors) {
-        console.log( "errors : " + errors );
+        console.log( "errors : " , errors );
     }
 };
 
