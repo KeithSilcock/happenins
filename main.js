97ef4bd301da6cc96683868113bde4bc358f33dd
Dylan Widjaja <dwidjaja945@gmail.com>
2018-04-17 12:22:19 -0700

    edited OOP version of yelpData. Awaiting for server to accept more arguments for more robust search

--- main.js
+++ main.js
@@ -5,7 +5,14 @@
 var searchFoodTerm;
 var foodLatitude;
 var foodLongitude;
-
+var yelpLocation;
+var yelpRadius;
+var yelpCategories;
+var yelpPrice;
+var yelpOpen_now;
+var yelpSort_by;
+const yelpBusinessResultsArray = [];
+const yelpLocationCoordinates = {};
 
 
 /***************************************************************************************************
@@ -24,61 +31,89 @@ var foodLongitude;
  */
 
 class yelpData {
-    constructor(term, latitude, longitude) {
-        this.searchTerm = term;
+    constructor(term, location, radius, categories, price, open_now, latitude, longitude, sort_by) { // we might not need any of these parameters and instead pull our data from DOM elements in global variables. This way every section does not have to be filled out.
+        this.term = term;
+        this.location = location;
+        this.radius = radius;
+        this.categories = categories;
+        this.price = price;
+        this.open_now = open_now;
         this.latitude = latitude;
         this.longitude = longitude;
-        this.yelpAjaxCall = {
-            dataType: "JSONP",
-            method: 'GET',
-            jsonCallBack : this.jsonCallBack(results),
-            // url: `https://api.yelp.com/v3/businesses/search?term=${searchFoodTerm}&latitude=${latitude}&longitude=${longitude}",    // for when we can input search terms and google map lat/long
-            url: "https://api.yelp.com/v3/businesses/search?term=bbq&latitude=34.0522&longitude=-118.2437",
-            data : {
-                "Authorization" : "Bearer 17TJfP0tFmBX3bHRcvUEDnVkR2VgnziO0jhDrwgPcrEJXjJ0H66V0H5kmMWQwTHX2cZfhynFzE3sjaEzBb-v7chrsyweKxQQIvPbbW5SvMZt01-PWWi7PPo2PEvVWnYx",
-                // "term" : "bbq",
-                // "latitude" : 34.0522,
-                // "longitude" : -118.2437,
-            },
-            success : function(results) {
-                console.log("success : " , results);
-            },
-            error : function(errors) {
-                console.log( "errors : " , errors );
-            }
-        }
+        this.sort_by = sort_by;
+        this.pullBusinessData = this.pullBusinessData.bind(this);
+        this.ajaxCall();
+
     }
-    jsonCallBack(jsonResults) {
-        console.log(jsonResults);
-    },
+    ajaxCall() {
+        var yelpAjaxCall = {
+            dataType: "JSON",
+            method: 'POST',
+            url: "http://yelp.ongandy.com/businesses",
+            data: {
+                "access_token": "17TJfP0tFmBX3bHRcvUEDnVkR2VgnziO0jhDrwgPcrEJXjJ0H66V0H5kmMWQwTHX2cZfhynFzE3sjaEzBb-v7chrsyweKxQQIvPbbW5SvMZt01-PWWi7PPo2PEvVWnYx",
+                "term": this.term,
+                "location" : this.location,
+                "radius" : this.radius,
+                "categories" : this.categories,
+                "price" : this.price,
+                "open_now" : this.open_now,
+                "latitude": this.latitude, //34.0522,
+                "longitude": this.longitude, //  -118.2437,
+                'sort_by' : this.sort_by
+            },
+            success: this.pullBusinessData,
+            error: function (errors) {
+                console.log("errors : ", errors);
+            }
+        };
+        $.ajax(yelpAjaxCall);
+    }
+    pullBusinessData(data) {
+        debugger;
+        yelpBusinessResultsArray.length = 0;
+        data.businesses.map( item => yelpBusinessResultsArray.push( item ) );
+        console.log(yelpBusinessResultsArray);
+        var {latitude, longitude} = data.region.center;
+        console.log(latitude, longitude);
+    }
+
 
 }
 
-var yelpAjaxCall = {
-    dataType: "JSONP",
-    method: 'GET',
-    // url: `https://api.yelp.com/v3/businesses/search?term=${searchFoodTerm}&latitude=${latitude}&longitude=${longitude}",
-    url: "https://api.yelp.com/v3/businesses/search?term=bbq&latitude=34.0522&longitude=-118.2437",
-    data : {
-        "Authorization" : "Bearer 17TJfP0tFmBX3bHRcvUEDnVkR2VgnziO0jhDrwgPcrEJXjJ0H66V0H5kmMWQwTHX2cZfhynFzE3sjaEzBb-v7chrsyweKxQQIvPbbW5SvMZt01-PWWi7PPo2PEvVWnYx",
-        // "term" : "bbq",
-        // "latitude" : 34.0522,
-        // "longitude" : -118.2437,
-    },
-    success : function(results) {
-        console.log("success : " , results);
-    },
-    error : function(errors) {
-        console.log( "errors : " , errors );
-    }
-};
+var newYelpCall = new yelpData("bbq", 34.0522, -118.2437);
 
-$.ajax(yelpAjaxCall);
+console.log(newYelpCall);
+
+// var yelpAjaxCall = {
+//     dataType: "JSON",
+//     method: 'POST',
+//     url: "http://yelp.ongandy.com/businesses",
+//     data : {
+//         "access_token" : "17TJfP0tFmBX3bHRcvUEDnVkR2VgnziO0jhDrwgPcrEJXjJ0H66V0H5kmMWQwTHX2cZfhynFzE3sjaEzBb-v7chrsyweKxQQIvPbbW5SvMZt01-PWWi7PPo2PEvVWnYx",
+//         "term" : "bbq",
+//         "latitude" : 34.0522,
+//         "longitude" : -118.2437,
+//     },
+//     success : function(results) {
+//         console.log("success : " , results);
+//         results.businesses.map( item => yelpBusinessResultsArray.push( item ) );
+//         console.log(yelpBusinessResultsArray);
+//         var {latitude, longitude} = results.region.center;
+//         console.log(latitude, longitude);
+//     },
+//     error : function(errors) {
+//         console.log( "errors : " , errors );
+//     }
+// };
+//
+// $.ajax(yelpAjaxCall);
+
 
 
 /***************************************************************************************************
  * callEventData
- * @params {string} input from the User to search through PredictHQ
+ * @para`ms {string} input from the User to search through PredictHQ
  * @returns: {object} data from PredictHQ
  * Sends request to PredictHQ API to pull data based off search input from User
  */
