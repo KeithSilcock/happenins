fd973e0784299d480d37d71deb163981717913be
Dylan Widjaja <dwidjaja945@gmail.com>
2018-04-17 08:51:56 -0700

    added jsdocumentation

---
+++ main.js
@@ -0,0 +1,37 @@
+/**
+ * Define all global variables here.
+ */
+
+/***************************************************************************************************
+ * initializeApp
+ * @params {undefined} none
+ * @returns: {undefined} none
+ * initializes the application: adds click handlers and pulls in data from server
+ */
+
+
+/***************************************************************************************************
+ * callYelpData
+ * @params {string} input from the User to search through Yelp
+ * @returns: {object} data from Yelp
+ * Sends request to Yelp API to pull data based off search input from User
+ */
+
+
+/***************************************************************************************************
+ * callEventData
+ * @params {string} input from the User to search through PredictHQ
+ * @returns: {object} data from PredictHQ
+ * Sends request to PredictHQ API to pull data based off search input from User
+ */
+
+
+/***************************************************************************************************
+ * renderInformationOnDOM
+ * @params {object} results from ajax calls
+ * @returns: {undefined}
+ * Takes the information from the ajax calls and displays on to DOM
+ */
+
+
+
