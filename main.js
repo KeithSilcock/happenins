05023ea7a3e147a3ee76202c55c747dfc70f7e45
sasanraf <sasanraf@yahoo.com>
2018-04-17 14:39:11 -0700

    returning object as event search result

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ $(document).ready(initializing);
 
 function initializing() {
     eventfulEventRequest();
+    //eventfulEventRequest(startDate, endDate, category)
 }
 
 /***************************************************************************************************
@@ -48,20 +49,70 @@ $.ajax(yelpAjaxCall);
 
 /***************************************************************************************************
  * callEventData
- * @params {string} input from the User to search through eventful
- * @returns: {object} data from eventful
+ * @params {string} startDate, endDate, category
+ * @returns: {object} object {city_name, description, title, venue_address, image-url-100px, image-url-250px, venue_name,
  * Sends request to eventful API to pull data based off search input from User
  */
-//function eventfulEventRequest(city,date){
+//function eventfulEventRequest(startDate, endDate, category){
 function eventfulEventRequest(){
+
+    var eventSearchResultArray = [];
+    var eventSearchResultObject = {};
+
     $.ajax({
-        url: "https://api.eventful.com/json/events/search?app_key=Zb7jwSS8MQppFwhH&location=los angeles&within=15&date='2018042000-2018042100';&page_size=50&category=new",
+        //url: "https://api.eventful.com/json/events/search?app_key=Zb7jwSS8MQppFwhH&location=los angeles&within=15&date="+  startDate +"00-" + endDate + "00&category=" +  category + "&image_sizes=blackborder250,block100&page_size=10&category=new",
+        url: "https://api.eventful.com/json/events/search?app_key=Zb7jwSS8MQppFwhH&location=los angeles&within=15&date=2018042000-2018042000&category=music&image_sizes=blackborder250,block100&page_size=20&category=new",
         dataType: 'jsonp',
         data: {},
-        success: function (data) {console.log(data) },
-        error: function (error) {console.log(error) },
+        success: function (rawData) {
+            for (var event=0; event<rawData.events.event.length; event++){
+                if (rawData.events.event[event].title !== null) {
+                    var title = rawData.events.event[event].title;
+                }
+                if (rawData.events.event[event].city_name !== null) {
+                    var cityName = rawData.events.event[event].city_name;
+                }
+                if (rawData.events.event[event].image !== null) {
+                    var imageSmallUrl = 'http:' + rawData.events.event[event].image.block100.url;
+                    var imageLargeUrl = 'http:' + rawData.events.event[event].image.blackborder250.url;
+                }
+                if (rawData.events.event[event].venue_address !== null) {
+                    var venue_address = rawData.events.event[event].venue_address;
+                }
+                if (rawData.events.event[event].venue_name!== null) {
+                    var venue_name = rawData.events.event[event].venue_name;
+                }
+                if (rawData.events.event[event].description !== null) {
+                    var description = rawData.events.event[event].description;
+                }
+
+                dataObject = {
+                    title: title,
+                    cityName: cityName,
+                    imageSmallUrl: imageSmallUrl,
+                    imageLargeUrl: imageLargeUrl,
+                    venue_address: venue_address,
+                    venue_name: venue_name,
+                    description: description
+                }
+
+                eventSearchResultArray.push(dataObject);
+            }
+
+
+
+            console.log(eventSearchResultArray);
+
+            return eventSearchResultArray;
+
+        },
+        error: function (error) { console.log(error) },
     });
+
+
+
 }
+
 
 
 /***************************************************************************************************
